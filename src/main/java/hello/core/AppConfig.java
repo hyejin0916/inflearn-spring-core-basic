67a4d93ad5f8ce7945d875efde51e1f1e2a93e57
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-19 16:16:03 +0900

    refactor: section4-19 apply separation of concerns

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig { // 애플리케이션 전체를 설정하고 구성한다
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                new MemoryMemberRepository(), new FixDiscountPolicy()
+        );
+    }
+}
