2ab51f1b0fc2989ba4fd59dcb5087cec9c6f4ff3
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-19 17:13:28 +0900

    refactor: section4-25 replace manual AppConfig wiring with Spring ApplicationContext

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,23 +7,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보를 관리하는 파일에서 @Configuration 작성
 public class AppConfig { // 애플리케이션 전체를 설정하고 구성한다
 
+    @Bean // Bean: 스프링 컨테이너에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(), discountPolicy()
         );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // 스프링 생성
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig에 있는 환경 설정 정보를 가지고 스프링이 @Bean이라 적힌 메서드들을 스프링 컨테이너에 넣어서 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 첫번째: 찾을 메서드명, 두번째: 타입
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
