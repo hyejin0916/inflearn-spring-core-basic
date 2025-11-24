461daf3ce57a3988452768d4f9b15f74b084b8d8
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-24 15:41:18 +0900

    test: section9-54 add lifecycle test using InitializingBean and DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,15 +1,16 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    // InitializingBean: 초기화 빈
 
     private String url;
 
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-//        disconnect();
     }
 
     // url은 외부에서 setter로 넣을 수 있게 설계
@@ -30,5 +31,20 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // afterPropertiesSet: 의존관계 주입이 끝나면 호출해주겠다.
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // destroy: 빈이 종료될 때 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
