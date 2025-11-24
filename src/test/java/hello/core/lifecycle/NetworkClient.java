fde5c838c2bcbbc9f49132de0a83ac0aaaa6f5ab
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-24 15:32:01 +0900

    test: section9-53 add bean lifecycle example separating creation and initialization

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,34 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+//        disconnect();
+    }
+
+    // url은 외부에서 setter로 넣을 수 있게 설계
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    // 연결한 서버에 메시지 보내기
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
