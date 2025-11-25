f3f5a0b022eb44975fcf5df21a8f776ec4dfae24
hyejin0916 <rkdgpwlsz@naver.com>
2025-11-25 16:41:22 +0900

    feat: section10-64 implement request scope example using scope proxyMode instead of ObjectProvider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,16 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     // private final MyLogger myLogger; 를 하면 request scope에서 에러 발생
     // ObjectProvider를 이용해 ObjectProvider.getObject()를 호출하는 시점까지 request scope 빈의 생성을 지연
+    private final MyLogger myLogger;
+    // proxyMode 추가 후 문제 해결
 
     @RequestMapping("log-demo")
     @ResponseBody // 문자를 그대로 응답으로 보냄
     public String logDemo(HttpServletRequest request) { // request 정보 받아옴
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
 
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
